f1d21f1fecd66dc65a7c52889a2c42489791cc98
FoxMaccloud <35312332+FoxMaccloud@users.noreply.github.com>
2020-06-17 15:46:06 +0200

    I'll explain this when I'm sober .. or revert it

--- updatePrograms.sh
+++ updatePrograms.sh
@@ -7,7 +7,12 @@ cd /home/foxmaccloud/Programs
 
 for dir in */ ; do
     echo -e "\nUpdateing: ${RED}$dir${NC}"
-    cd "$dir" && git pull -q --progress
-    sleep 1
+    cd "$dir"
+    if [ -d ".git" ]; then
+        git pull -q --progress
+    else
+        echo -e "${RED}Error:${NC} Not a git repository!"
+    fi
+    sleep 0.1
     cd ..
 done
